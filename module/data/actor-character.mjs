8985610cea33ef471845b2d904f96960bc2ac6c3
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-11-30 17:17:34 +0000

    Add basic pc datastructure

--- module/data/actor-character.mjs
+++ module/data/actor-character.mjs
@@ -7,46 +7,54 @@ export default class lwfCharacter extends lwfActorBase {
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
+    // All attributes specific to Lone WOlf fists, affected by levelling up, and not covered by the other categories
     schema.attributes = new fields.SchemaField({
-      level: new fields.SchemaField({
+      degree: new fields.SchemaField({
         value: new fields.NumberField({ ...requiredInteger, initial: 1 })
       }),
+      aura: new fields.NumberField({ ...requiredInteger, initial: 1, min: 0}),
+      effortless: new fields.NumberField({ ...requiredInteger, initial: 1}),
+      foci: new fields.NumberField({ ...requiredInteger, initial: 1}),
+      masteries: new fields.NumberField({ ...requiredInteger, initial: 1})
     });
 
-    // Iterate over ability names and create a new SchemaField for each.
-    schema.abilities = new fields.SchemaField(Object.keys(CONFIG.LWF.abilities).reduce((obj, ability) => {
-      obj[ability] = new fields.SchemaField({
-        value: new fields.NumberField({ ...requiredInteger, initial: 10, min: 0 }),
-      });
-      return obj;
-    }, {}));
+    // All the relevant datafields relating to chakras and Prana generation
+    schema.chakra = new fields.SchemaField({
+      prana: new fields.SchemaField ({
+        value: new fields.NumberField({ ...requiredInteger, initial: 0, min: 0, max: 100})
+      }),
+      active: new fields.SchemaField ({
+        value: new fields.NumberField({ ...requiredInteger, initial: 1, min: 1, max: 7}),
+        initial: new fields.NumberField({ ...requiredInteger, initial: 1, min: 1, max: 7}),
+      }),
+      recovery: new fields.NumberField({ ...requiredInteger, initial: 3})
+    })
+
+    schema.karma = new fields.SchemaField({
+      current: new fields.NumberField({ ...requiredInteger, initial: 0}),
+      next: new fields.NumberField({ ...requiredInteger, initial: 200}),
+      spent: new fields.NumberField({ ...requiredInteger, initial: 120})
+    })
 
     return schema;
   }
 
   prepareDerivedData() {
     // Loop through ability scores, and add their modifiers to our sheet output.
+    /*
     for (const key in this.abilities) {
       // Calculate the modifier using d20 rules.
       this.abilities[key].mod = Math.floor((this.abilities[key].value - 10) / 2);
       // Handle ability label localization.
       this.abilities[key].label = game.i18n.localize(CONFIG.LWF.abilities[key]) ?? key;
-    }
+    }*/
   }
 
   getRollData() {
     const data = {};
 
-    // Copy the ability scores to the top level, so that rolls can use
-    // formulas like `@str.mod + 4`.
-    if (this.abilities) {
-      for (let [k,v] of Object.entries(this.abilities)) {
-        data[k] = foundry.utils.deepClone(v);
-      }
-    }
-
-    data.lvl = this.attributes.level.value;
-
+    // Copy effort to the top level, so it can be used by the standard rolls.
+    data.effort = this.power.value;
     return data
   }
 }
