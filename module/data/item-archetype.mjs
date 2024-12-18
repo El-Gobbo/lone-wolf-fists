58c5816217e69d10011795724810c0cf02ebf228
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-12-18 20:50:43 +0000

    Remove unnecessary imports

--- module/data/item-archetype.mjs
+++ module/data/item-archetype.mjs
@@ -3,17 +3,11 @@ import lwfItemBase from "./base-item.mjs";
 export default class lwfArchetype extends lwfItemBase {
 
   static defineSchema() {
-    const { SchemaField, BooleanField, NumberField, StringField, ArrayField, HTMLField } = foundry.data.fields;
-    const requiredInteger = { required: true, nullable: false, integer: true };
+    const { StringField } = foundry.data.fields;
     const schema = super.defineSchema();
 
     schema.archetype = new StringField();
 
     return schema;
   }
-  prepareDerivedData() {
-    this.rank = Math.floor(this.agg / 10);
-
-  }
-
 }
