7decf0fce51eb4b07667b7a853a72b192b7feccf
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2025-01-18 11:09:45 +0000

    Implement worn check on armor on character sheet

--- module/data/item-armor.mjs
+++ module/data/item-armor.mjs
@@ -3,11 +3,12 @@ import lwfItemBase from "./base-item.mjs";
 export default class lwfArmor extends lwfItemBase {
 
   static defineSchema() {
-    const { NumberField } = foundry.data.fields;
+    const { NumberField, BooleanField } = foundry.data.fields;
     const requiredInteger = { required: true, nullable: false, integer: true };
     const schema = super.defineSchema();
 
-    schema.armor = new NumberField({ ...requiredInteger, initial: 0});
+    schema.armorValue = new NumberField({ ...requiredInteger, initial: 0});
+    schema.worn = new BooleanField({initial: false});
     
     return schema;
   }
