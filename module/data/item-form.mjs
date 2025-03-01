461fad65a199ead0bdeef31a0641b7463b036985
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2025-03-01 14:28:11 +0000

    Improve the form sheet and create techniques pack

--- module/data/item-form.mjs
+++ module/data/item-form.mjs
@@ -6,22 +6,24 @@ export default class lwfForm extends lwfYuddhakala {
     const { SchemaField, NumberField, StringField, BooleanField } = foundry.data.fields;
     const schema = super.defineSchema();
 
+    schema.requirements = new StringField({ initial: "" });
     schema.attackBoost = new SchemaField({
+      name: new StringField(),
       min: new NumberField({min: 0, max: 9}),
       max: new NumberField({min: 0, max: 9}),
-      none: new BooleanField({initial: false}),
+      to: new StringField(),
+      none: new BooleanField({ initial: false }),
+      doubled: new BooleanField({ initial: false })
+
     });
     schema.defenseBoost = new SchemaField({
+      name: new StringField(),
       min: new NumberField({min: 0, max: 9}),
       max: new NumberField({min: 0, max: 9}),
-      none: new BooleanField({initial: false}),
+      to: new StringField(),
+      none: new BooleanField({ initial: false }),
+      doubled: new BooleanField({ initial: false })
     });
-    schema.otherBoost = new SchemaField({
-      facing: new NumberField({min: 0, max: 9}),
-      description: new StringField (),
-      none: new BooleanField({initial: false}),
-    });
-
     return schema;
   }
 
